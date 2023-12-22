3f625d9e0297ddc1e2ff591c65f9b2043f832908
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-12-22 17:00:10 +0100

    fix: footer props

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -6,13 +6,86 @@ export default {
     components: {
         Topics,
         FooterLinks,
+    },
+    data() {
+        return {
+            sections: [
+                {
+                    title: 'Avada tech forum',
+                    paragraph: 'Sed semper, nisl id feugiat eleifend, erat est digssim felis, quis auctor elit enim nec ante. Cras et nibh risus. Pellentesque non mi sit amet mi intermentum.',
+                },
+                {
+                    title: 'Popular Topics',
+                    topics: [
+                        {
+                            title: 'Nam a dolor voluptat massa auctor semper',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Sed euismond nunc urna fermentum arcu dapibus fringilla.',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Curabitur a scelerisque ligula',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                    ]
+                },
+                {
+                    title: 'Recent Topics',
+                    topics: [
+                        {
+                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Proin at ligula sagittis vestibulum nisi vitae',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Sed nec metus at est tincidunt elementum',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Nulla egestas nulla eu nulla suscipit molestie',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                    ]
+                },
+                {
+                    title: 'Latest Replies',
+                    topics: [
+                        {
+                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Nulla egestas nulla eu nulla suscipit molestie',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                        {
+                            title: 'Mauris ac nibh quis eros sagittis lacinia a et dui',
+                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
+                        },
+                    ]
+                },
+            ]
+        }
     }
 };
 </script>
 
 <template>
     <footer>
-        <Topics />
+        <Topics :sections="this.sections" />
         <FooterLinks />
     </footer>
 </template>
--- src/components/Footer/Topics.vue
+++ src/components/Footer/Topics.vue
@@ -1,79 +1,7 @@
 <script>
 export default {
     name: 'Topics Section',
-    data() {
-        return {
-            sections: [
-                {
-                    title: 'Avada tech forum',
-                    paragraph: 'Sed semper, nisl id feugiat eleifend, erat est digssim felis, quis auctor elit enim nec ante. Cras et nibh risus. Pellentesque non mi sit amet mi intermentum.',
-                },
-                {
-                    title: 'Popular Topics',
-                    topics: [
-                        {
-                            title: 'Nam a dolor voluptat massa auctor semper',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Sed euismond nunc urna fermentum arcu dapibus fringilla.',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Curabitur a scelerisque ligula',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                    ]
-                },
-                {
-                    title: 'Recent Topics',
-                    topics: [
-                        {
-                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Proin at ligula sagittis vestibulum nisi vitae',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Sed nec metus at est tincidunt elementum',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Nulla egestas nulla eu nulla suscipit molestie',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                    ]
-                },
-                {
-                    title: 'Latest Replies',
-                    topics: [
-                        {
-                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Cras lacinia tellus id mauris finibus lacus molestie',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Nulla egestas nulla eu nulla suscipit molestie',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                        {
-                            title: 'Mauris ac nibh quis eros sagittis lacinia a et dui',
-                            paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut itaque ullam quo nam doloribus vero dolore voluptatum! Voluptatibus similique saepe omnis sit incidunt expedita? Laudantium porro repudiandae illum deserunt distinctio.',
-                        },
-                    ]
-                },
-            ]
-        }
-    }
+    props: ['sections',],
 };
 </script>
 
@@ -81,7 +9,7 @@ export default {
     <section>
         <div class="container">
             <ul class="row g-5">
-                <li v-for="section in this.sections" class="col-3">
+                <li v-for="section in sections" class="col-3">
                     <div>
                         <h4>{{ section.title }}</h4>
                         <p v-if="section.paragraph">{{ section.paragraph }}</p>
